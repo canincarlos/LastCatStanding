80e64a47c29ec715ed539e964666cea9512ec60b
Canin Apriori <canin@Canins-MacBook-Pro.local>
2022-09-26 08:01:31 -0500

    updating the LCS/About view

--- Last Cat Standing/src/views/AboutView.vue
+++ Last Cat Standing/src/views/AboutView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
     <h1>This is the winners page</h1>
+    <p>Or at least it will be ... Though right now it's just an about page.</p>
+    <p>This app began as a take-home coding examt that I'm continuing to use as a test app for new JavaScript and Front-end technologies; primarily following the VueJS framework. Though there is a chance a NextJS/React version will be made as well.</p>
   </div>
 </template>
 
